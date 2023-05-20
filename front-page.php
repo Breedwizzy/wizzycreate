5d64df45982a7e42815dd0e3c1a0960da7ec8b1f
Breedwizzy <breedwizzy@yahoo..com>
2023-05-20 08:09:45 +0100

    first commit

---
+++ front-page.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Blog Site Template</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Blog Site Template">
+    <meta name="author" content="https://youtube.com/FollowAndrew">
+    <link rel="shortcut icon" href="images/logo.png">
+
+    <?php
+    wp_head();
+    ?>
+
+</head>
+
+<body>
+
+
+    <div class="main-wrapper">
+
+
+        <header class="page-title "> <!-- theme-bg-dark text-center gradient py-5 -->
+
+            <nav>
+
+                <a href="#">
+                    <img class='ibl-logo'
+                        src="https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/logo_v2.png"
+                        width="300" alt="" class="image-7">
+                </a>
+
+                <ul class='nav-ul'>
+                    <li><a href="#">AI Mentor</a></li>
+                    <li><a href="#">AI Analytics</a></li>
+                    <li><a href="#">AI Skills</a></li>
+                    <li><a href="#">AI Studio</a></li>
+                    <li class="dropdown">
+                        <a href="#">About</a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">About IBL</a></li>
+                            <li><a href="#">Testimonials</a></li>
+                        </ul>
+                    </li>
+                    <a href="#">
+                        <img class="github-logo aws-logo grey-logo" href="#"
+                            src='https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/github-1.png'
+                            width="30">
+                    </a>
+
+                    <a href="#">
+                        <img class="lazyload aws-logo grey-logo" href="#"
+                            src='https://ibleducation.com/wp-content/themes/ibl-web-ibleducation-theme/img/aws-logo-c.png'
+                            width="50">
+                    </a>
+
+                    <li class='contactus-class'>
+                        <a href="#">Contact Us</a>
+                    </li>
+
+                </ul>
+                <label for='check' class='checkbtn'>
+                    <i class='fas fa-bars'></i>
+
+                </label>
+            </nav>
+
+            <hr>
+
+        </header>
+
+
+
+
+
+
+
+
+
+
+        <footer class="footer text-center py-2 theme-bg-dark">
+
+            <p class="copyright"><a href="">Wizzycreate</a></p>
+
+        </footer>
+
+
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+    <?php
+    wp_footer();
+    ?>
+
+</body>
+
+</html>
